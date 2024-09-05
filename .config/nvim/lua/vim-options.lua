d678be98efab9df5ca8aed6de2990730cc23461c
AnkushRoy-code <ankush3411111@gmail.com>
2024-09-05 14:38:02 +0530

    Rofi-emoji

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -1,7 +1,7 @@
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
 
 vim.opt.number = true
 vim.opt.relativenumber = true
