a157aba374cef6e5379db8f312e07e56d09c4f23
AnkushRoy-code <ankush3411111@gmail.com>
2024-10-26 17:37:30 +0530

    Did some work in nvim
    My repos aren't supposed to be professional, description do not have
    anything good.

--- .config/nvim/lua/plugins/efficiency.lua
+++ .config/nvim/lua/plugins/efficiency.lua
@@ -18,6 +18,11 @@ return { -- This file contains plugins that help me navigate/edit files faster.
 		"kylechui/nvim-surround",
 		version = "*", -- Use for stability; omit to use `main` branch for the latest features
 		event = { "BufReadPost", "BufNewFile" },
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
 	},
 
 	{
@@ -27,11 +32,19 @@ return { -- This file contains plugins that help me navigate/edit files faster.
 		event = { "BufReadPost", "BufNewFile" },
         -- stylua: ignore
         keys = {
-            { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+            { "k",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
             { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
             { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
             { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
             { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
         },
+	},
+
+	{
+		"junegunn/vim-easy-align",
+		enabled = true,
+		keys = {
+			{ "ga", mode = { "x", "n" }, "<Plug>(EasyAlign)", desc = "Easy allign" },
+		},
 	},
 }
